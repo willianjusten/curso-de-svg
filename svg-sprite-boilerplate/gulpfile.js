46f138357737f982ab333d6965a112c062284632
Willian Justen <willianjustenqui@gmail.com>
2016-05-23 19:53:15 -0300

    Add gulp boilerplate

---
+++ svg-sprite-boilerplate/gulpfile.js
@@ -0,0 +1,23 @@
+// declarar as nossas dependências
+var gulp = require('gulp');
+var svgSprite = require('gulp-svg-sprite');
+
+var config = {
+    mode: {
+        symbol: {
+            dest: 'sprite',
+            sprite: 'sprite.svg',
+            example: true
+        },
+        svg: {
+            xmlDeclaration: false,
+            doctypeDeclaration: false
+        }
+    }
+};
+
+gulp.task('sprites', function(){
+    return gulp.src('icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('.'))
+});
